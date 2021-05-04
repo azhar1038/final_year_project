6c586580304e13e2aeb7d0672270bba6b49af291
SoumyaRanjanNaik <srnaik2209@gmail.com>
2021-05-04 22:50:56 +0530

    side by side image comparison implemented

--- app/views.py
+++ app/views.py
@@ -1,13 +1,25 @@
+import io
 from app.utils import load_and_generate
 from django.shortcuts import render
 from django.http import HttpResponse
+import base64
 
 # Create your views here.
 def pred_view(request):
     if request.method == "POST":
-        op = load_and_generate(request.FILES.get("image").file.name, False)
-        response = HttpResponse(content_type="image/jpg")
-        op.save(response, "JPEG")
-        return response
+        op = load_and_generate(request.FILES.get("image").file.name, resize=False)
+        response = io.BytesIO()
+        op.save(response, "gif")
+        # return response
+        return render(
+            request,
+            "home.html",
+            context={
+                "inp": base64.b64encode(
+                    open(request.FILES.get("image").file.name, "rb").read()
+                ).decode("utf-8"),
+                "out": base64.b64encode(response.getvalue()).decode("utf-8"),
+            },
+        )
     else:
         return render(request, "home.html")
